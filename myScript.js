16fbe5cf67adae8a8d64765ef44549bd4f1a0b2b
00tranluc <00tranluc@gmail.com>
2018-08-01 11:32:45 -0700

    second commit

---
+++ myScript.js
@@ -0,0 +1,19 @@
+function answer() {
+  document.getElementById("question").innerHTML = "po-ta-to chip";
+}
+
+function firstimage() {
+  document.getElementById("imagess").src="http://www.animalspot.net/wp-content/uploads/2011/08/American-Robin-Images.jpg";
+}
+
+function secondimage() {
+  document.getElementById("imagess").src="https://d1ia71hq4oe7pn.cloudfront.net/photo/60411301-720px.jpg";
+}
+
+function american() {
+  document.getElementById("second").style.display="none";
+}
+
+function goldfinch() {
+  document.getElementById("second").style.display="block";
+}
